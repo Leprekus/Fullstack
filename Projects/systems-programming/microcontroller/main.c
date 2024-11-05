065cd9c18cd071fe78915d21313df4e6a6ce70de
leprekus <leprekus@yahoo.com>
2024-11-05 07:09:03 -0800

    initial commit

---
+++ Projects/systems-programming/microcontroller/main.c
@@ -0,0 +1,14 @@
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+//the following are in bytes
+const int WORD_SIZE = 8;
+const int FLASH_MEM_SIZE = 256; // size of addressable memory
+const int SRAM_SIZE = 64; // size of stack
+
+
+typedef struct {
+	uint:w
+} CPU_REGISTERS;
+int main() {
+	return 0;
+}
